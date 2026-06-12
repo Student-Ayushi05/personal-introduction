ae1c682feb427de4cdbb8aca5ba5715f3dcc3455
Student-Ayushi05 <ayushikhichi99@gmail.com>
2026-06-13 02:51:57 +0530

    Add files via upload

---
+++ personal_intro.py
@@ -0,0 +1,15 @@
+# Personal Introduction 
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+hobby = input("What is your hobby: ")
+
+#Friendly Welcome Message!
+
+print("\n 🎉---Welcome---🎉")
+print(f"Hello", name,"!")
+print(f"You are", age, "years old.")
+print(f"It's great to know that you play", hobby,".")
+print("Have a Fantastic Day!")
+
+input("\nPress Enter to exit...")
